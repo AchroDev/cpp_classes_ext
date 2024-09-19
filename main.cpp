3a765bc8382092964388b0aeec37fbcea1479091
AchroDev <achrodev@proton.me>
2024-09-19 00:07:57 -0500

    Added example calls and template classes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+class Log()
+{
+public:
+    void SetLevel(int level)
+    {
 
+    }
+
+    void Warn(const char* message)
+    {
+
+    }
+
+};
 
 int main(){
-    
+    Log log; // Initiating the log
+    log.SetLevel(LogLevelWarning); // Setting a warning level for the log messages
+    log.Warn("AHHH!"); // Example/Test warning message
     std::cin.get();
 }
